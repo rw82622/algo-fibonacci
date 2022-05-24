e7404423d41f066a3b0df93dc9ade7c92148a58d
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-24 14:46:42 -0500

    initial js solution for fibonacci

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,10 @@
 const fibonacci = (num) => {
-    
-}
+  if (num == 1) {
+    return 1;
+  } else if (num == 0) {
+    return 0;
+  }
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
 
-module.exports = {fibonacci}
+module.exports = { fibonacci };
